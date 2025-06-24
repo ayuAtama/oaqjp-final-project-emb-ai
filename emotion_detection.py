456d2e05ff3c51cab0a2a253b11b79583b57d1b9
Wahyu Pratama <youremail@example.com>
2025-06-24 00:38:04 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,17 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = headers)
-    return response.text
+    formatted_json = response.json()
+    formatted_response = formatted_json["emotionPredictions"][0]["emotion"]
+    # print(formatted_json["emotionPredictions"][0]["emotion"])
+    dominant_emotion = max(formatted_response, key=formatted_response.get)
+    formatted_response['dominant_emotion'] = dominant_emotion
+    # print(formatted_response)
+    return formatted_response
+
+# emotion_detector("I hate this new technology.")
